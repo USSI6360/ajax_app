0059512cf48d0cf1c3091bf2dcecdb6b51b90e2d
USSI6360 <kudan.mugen@gmail.com>
2020-10-07 08:40:41 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,9 @@
 class PostsController < ApplicationController
 
   def index  # indexアクションを定義した
-    @posts = Post.all  # 1番目のレコードを@postに代入
+    @posts = Post.all.order(id: "DESC")
   end
-  def new
-  end
+  
 
   def create
     Post.create(content: params[:content])
